6180a5529800035782b3eb91f8f580b5d54f2931
Mary Karakida <makarakida@gmail.com>
2022-07-21 12:15:22 -0300

    fix: GET /categories not returning games list if name query string is empty

--- src/controlleres/gamesController.js
+++ src/controlleres/gamesController.js
@@ -4,10 +4,18 @@ export async function listGames(req, res) {
     const { name } = req.query;
 
     try {
-        const games = await connection.query(
-            'SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON "categoryId" = categories.id WHERE LOWER(games.name) LIKE LOWER($1 || \'%\')',
-            [name]
-        );
+        let games;
+
+        if (name) {
+            games = await connection.query(
+                'SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON "categoryId" = categories.id WHERE LOWER(games.name) LIKE LOWER($1 || \'%\')',
+                [name]
+            );
+        } else {
+            games = await connection.query(
+                'SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON "categoryId" = categories.id'
+            );
+        }
 
         res.status(200).send(games.rows);
     } catch (err) {
