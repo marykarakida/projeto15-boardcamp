8076a0ac41420303fee373e3ab60dede5e59666c
Mary Karakida <makarakida@gmail.com>
2022-07-25 11:42:09 -0300

    fix: POST /customers now verify if birthday date is valid and customer age is equal or greater than 15

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,4 +1,8 @@
 import joi from 'joi';
+import dayjs from 'dayjs';
+
+const currentDate = dayjs();
+const maxDate = new Date(currentDate - 1000 * 60 * 60 * 24 * 365 * 15);
 
 const customerSchema = joi.object({
     name: joi.string().required(),
@@ -13,7 +17,7 @@ const customerSchema = joi.object({
         .length(11)
         .pattern(/^[0-9]*$/)
         .required(),
-    birthday: joi.date().required()
+    birthday: joi.date().max(maxDate).required()
 });
 
 export default customerSchema;
